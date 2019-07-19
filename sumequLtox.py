9769f347b2a186215d323a0295f76806969f34ee
reshmahayathbi1999 <50619287+reshmahayathbi1999@users.noreply.github.com>
2019-07-19 09:16:03 +0530

    Create sumequLtox.py

---
+++ sumequLtox.py
@@ -0,0 +1,12 @@
+a1,b=map(int,input().split())
+o=list(map(int,input().split()))
+v=0
+for i in range(0,len(o)):
+    for j in range(1,len(o)):
+        sum=o[i]+o[j]
+        if sum==b:
+            v=1
+if v==1:
+    print("yes")
+else:
+    print("no")
